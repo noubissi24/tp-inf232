03ae5ca64cab42c349178ee35839fef16025523e
noubissi24 <noubissiyannolf@gmail.com>
2026-04-27 22:43:18 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const apiRoutes = require('./routes/api');
+app.use('/api', apiRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`Serveur sur http://localhost:${PORT}`));
